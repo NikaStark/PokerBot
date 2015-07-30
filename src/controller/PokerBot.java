1d6a7397806749db89820f7906e8034ce09c74a7
NikaStark <k7a3r5@gmail.com>
2015-07-30 16:26:48 +0300

    Add class PokerBot

---
+++ src/controller/PokerBot.java
@@ -0,0 +1,15 @@
+package controller;
+
+public class PokerBot {
+
+    public static final int AMOUNT_CARDS_AT_PLAYER = 2;
+
+    public static final int AMOUNT_CARDS_AT_FLOP = 3;
+
+    public static final int AMOUNT_CARDS_AT_DISTRIBUTION = 7;
+
+    public static final int MAX_NUMERIC_VALUE_CARD = 10;
+
+    public static final int MIN_NUMERIC_VALUE_CARD = 2;
+
+}
