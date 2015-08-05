e39810cd9771b43f26f273eb7c697ddbf68e15f5
NikaStark <k7a3r5@gmail.com>
2015-08-05 13:59:27 +0300

    Add deck of cards and initialize it

--- src/controller/PokerBot.java
+++ src/controller/PokerBot.java
@@ -1,5 +1,9 @@
 package controller;
 
+import models.Card;
+import models.Enums.HighCard;
+import models.Enums.Suits;
+
 public class PokerBot {
 
     public static final int AMOUNT_CARDS_AT_PLAYER = 2;
@@ -11,5 +15,23 @@ public class PokerBot {
     public static final int MAX_NUMERIC_VALUE_CARD = 10;
 
     public static final int MIN_NUMERIC_VALUE_CARD = 2;
+
+    public static final int COUNT_CARDS_IN_DECK = 52;
+
+    public static final Card[] DECK_OF_CARDS = new Card[COUNT_CARDS_IN_DECK];
+
+    static {
+        int index = 0;
+        for (int i = MIN_NUMERIC_VALUE_CARD; i <= MAX_NUMERIC_VALUE_CARD; i++) {
+            for (Suits suit : Suits.values()) {
+                DECK_OF_CARDS[index++] = new Card(i, suit);
+            }
+        }
+        for (HighCard highCard : HighCard.values()) {
+            for (Suits suit : Suits.values()) {
+                DECK_OF_CARDS[index++] = new Card(highCard, suit);
+            }
+        }
+    }
 
 }
