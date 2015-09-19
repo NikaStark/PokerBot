f626fa836b7553bce8b2d4c619cdf84befed24a2
NikaStark <k7a3r5@gmail.com>
2015-09-19 23:04:20 +0300

    Rename class PokerBot to Game,  and rename some fields

--- src/models/Game.java
+++ src/models/Game.java
@@ -1,24 +1,23 @@
-package controller;
+package models;
 
-import models.Card;
 import models.Enums.HighCard;
 import models.Enums.Suits;
 
-public class PokerBot {
-
-    public static final int COUNT_CARDS_AT_PLAYER = 2;
-
-    public static final int COUNT_CARDS_AT_FLOP = 3;
-
-    public static final int COUNT_CARDS_AT_DISTRIBUTION = 7;
+public class Game {
 
     public static final int MAX_NUMERIC_VALUE_CARD = 10;
 
     public static final int MIN_NUMERIC_VALUE_CARD = 2;
 
-    public static final int COUNT_CARDS_IN_DECK = 52;
+    public static final int AMOUNT_CARDS_AT_PLAYER = 2;
 
-    public static final Card[] DECK_OF_CARDS = new Card[COUNT_CARDS_IN_DECK];
+    public static final int AMOUNT_CARDS_AT_FLOP = 3;
+
+    public static final int AMOUNT_CARDS_AT_DISTRIBUTION = 7;
+
+    public static final int AMOUNT_CARDS_IN_DECK = 52;
+
+    public static final Card[] DECK_OF_CARDS = new Card[AMOUNT_CARDS_IN_DECK];
 
     static {
         int index = 0;
