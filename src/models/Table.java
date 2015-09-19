6066af4c6bd4a48115bf639c7bb84f988f3e44fa
NikaStark <k7a3r5@gmail.com>
2015-09-19 22:58:57 +0300

    Initialise class Table

---
+++ src/models/Table.java
@@ -0,0 +1,13 @@
+package models;
+
+import controller.Calculator;
+
+public class Table {
+
+    final Calculator calculator;
+
+    public Table(final Calculator calculator) {
+        this.calculator = calculator;
+    }
+
+}
