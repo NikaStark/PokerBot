0aeec73de6abad6c3c0679f0dcc3ffa352d25f34
NikaStark <k7a3r5@gmail.com>
2015-09-22 04:35:30 +0300

    CardTest - add tests for "equals" method

--- Tests/models/CardTest.java
+++ Tests/models/CardTest.java
@@ -34,4 +34,57 @@ public class CardTest {
         final Suits actualAnswer = card.getSuit();
         assertEquals(expectedAnswer, actualAnswer);
     }
+
+    @Test
+    public void testEqualsLessThenMaxNumericValueWhenEqual() throws Exception {
+        final Card card1 = new Card(7, Suits.Spades);
+        final Card card2 = new Card(7, Suits.Spades);
+        final boolean expectedAnswer = true;
+
+        final boolean actualAnswer1 = card1.equals(card2);
+        assertEquals(expectedAnswer, actualAnswer1);
+
+        final boolean actualAnswer2 = card2.equals(card1);
+        assertEquals(expectedAnswer, actualAnswer2);
+    }
+
+    @Test
+    public void testEqualsMoreThenMaxNumericValueWhenEqual() throws Exception {
+        final Card card1 = new Card(HighCard.King, Suits.Spades);
+        final Card card2 = new Card(HighCard.King, Suits.Spades);
+        final boolean expectedAnswer = true;
+
+        final boolean actualAnswer1 = card1.equals(card2);
+        assertEquals(expectedAnswer, actualAnswer1);
+
+        final boolean actualAnswer2 = card2.equals(card1);
+        assertEquals(expectedAnswer, actualAnswer2);
+    }
+
+    @Test
+    public void testEqualsLessThenMaxNumericValueWhenNotEqual() throws Exception {
+        final Card card1 = new Card(3, Suits.Spades);
+        final Card card2 = new Card(6, Suits.Spades);
+        final boolean expectedAnswer = false;
+
+        final boolean actualAnswer1 = card1.equals(card2);
+        assertEquals(expectedAnswer, actualAnswer1);
+
+        final boolean actualAnswer2 = card2.equals(card1);
+        assertEquals(expectedAnswer, actualAnswer2);
+    }
+
+    @Test
+    public void testEqualsMoreThenMaxNumericValueWhenNotEqual() throws Exception {
+        final Card card1 = new Card(HighCard.King, Suits.Spades);
+        final Card card2 = new Card(HighCard.King, Suits.Clubs);
+        final boolean expectedAnswer = false;
+
+        final boolean actualAnswer1 = card1.equals(card2);
+        assertEquals(expectedAnswer, actualAnswer1);
+
+        final boolean actualAnswer2 = card2.equals(card1);
+        assertEquals(expectedAnswer, actualAnswer2);
+    }
+
 }
