5e385f97e0aa091c5be239200a81756a6161e376
NikaStark <k7a3r5@gmail.com>
2015-10-01 08:26:07 +0300

    Suits - add overriding method toString

--- src/models/Enums/Suits.java
+++ src/models/Enums/Suits.java
@@ -2,6 +2,19 @@ package models.Enums;
 
 public enum Suits {
 
-    Spades, Hertz, Clubs, Diamonds
+    Spades, Hertz, Clubs, Diamonds;
+
+    @Override
+    public String toString() {
+        if (this == Spades) {
+            return "Spades";
+        } else if (this == Hertz) {
+            return "Hertz";
+        } else if (this == Clubs) {
+            return "Clubs";
+        } else {
+            return "Diamonds";
+        }
+    }
 
 }
