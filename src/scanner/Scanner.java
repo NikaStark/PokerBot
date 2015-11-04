6e90ba1d5716c32b5f9a29d570a2a784b1ccf923
NikaStark <k7a3r5@gmail.com>
2015-11-04 03:10:54 +0200

    Scanner - add method for reading file from the end, method what recognized when scanner must reading

--- src/scanner/Scanner.java
+++ src/scanner/Scanner.java
@@ -1,5 +1,55 @@
 package scanner;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.RandomAccessFile;
+import java.util.Date;
+
 public class Scanner {
+
+
+    public boolean activeWindow(final File fileInput) throws IOException {
+        Date currentDate = new Date();
+        Date fileDate = new Date(fileInput.lastModified());
+        if (currentDate.after(fileDate)) {
+            try (RandomAccessFile randomAccessFile = new RandomAccessFile(fileInput, "r")) {
+                String currentLastLine = readReverseFile(randomAccessFile);
+                if (currentLastLine.charAt(0) == ' ' && currentLastLine.charAt(1) == '\'' &&
+                        currentLastLine.charAt(2) == '*') {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public String readReverseFile(RandomAccessFile inputFile) throws IOException {
+        StringBuilder line = new StringBuilder();
+        boolean eol = false;
+        long readerPointer;
+        int symbol;
+        if ((readerPointer = inputFile.getFilePointer()) == 0) {
+            readerPointer = inputFile.length() - 1;
+        }
+        while (!eol) {
+            inputFile.seek(readerPointer);
+            switch (symbol = inputFile.read()) {
+                case '\r':
+                case '\n':
+                    if (!line.toString().isEmpty()) {
+                        eol = true;
+                    }
+                    break;
+                default:
+                    line.append((char)symbol);
+                    break;
+            }
+            readerPointer--;
+        }
+        inputFile.seek(readerPointer);
+        return line.reverse().toString();
+    }
 
 }
