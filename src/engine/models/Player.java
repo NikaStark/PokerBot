db5f056b56f7a9beddc6d3e5b74d8dbd42ac98a1
NikaStark <k7a3r5@gmail.com>
2015-11-09 00:12:50 +0200

    Player - create constructors, setters and getters

--- src/engine/models/Player.java
+++ src/engine/models/Player.java
@@ -2,6 +2,29 @@ package engine.models;
 
 public class Player {
 
+    private boolean statusIsActive;
 
+    private boolean isItMySit;
+
+    public Player() {
+        this(false, false);
+    }
+
+    public Player(boolean statusIsActive, boolean isItMySit) {
+        this.statusIsActive = statusIsActive;
+        this.isItMySit = isItMySit;
+    }
+
+    public boolean isStatusIsActive() {
+        return statusIsActive;
+    }
+
+    public void setStatusIsActive(boolean statusIsActive) {
+        this.statusIsActive = statusIsActive;
+    }
+
+    public boolean isItMySit() {
+        return isItMySit;
+    }
 
 }
