78b6d0898483cf0a2230bcabbe828789b3e354c3
NikaStark <k7a3r5@gmail.com>
2015-11-10 22:55:36 +0200

    Scanner - add isTableNew

--- src/scanner/Scanner.java
+++ src/scanner/Scanner.java
@@ -1,5 +1,8 @@
 package scanner;
 
+import engine.models.Game;
+import engine.models.Table;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -10,12 +13,12 @@ import java.util.Date;
 
 public class Scanner {
 
-    public boolean scanner(File fileInput) {
+    public boolean scanner(File fileInput, Game game) {
         while (true) {
             try (RandomAccessFile randomAccessFile = new RandomAccessFile(fileInput, "r")) {
                 if (timeToReading(fileInput)) {
 //                    long t1=System.nanoTime();
-                    readingLogs(randomAccessFile);
+                    readingLogs(randomAccessFile, game);
 //                    long t2 = System.nanoTime();
 //                    long elapsedTime = t2-t1;
 //                    System.out.println("Elapsed time was "+elapsedTime+" ns");
@@ -35,7 +38,7 @@ public class Scanner {
     }
 
 
-    public void readingLogs(RandomAccessFile randomAccessFile) throws IOException {
+    public void readingLogs(RandomAccessFile randomAccessFile, Game game) throws IOException {
         final String pathOutput = "C:\\Users\\Alex\\Desktop\\logFile3.3.0";
         ArrayList<String> tempStorage = new ArrayList<>();
         String tempString;
@@ -44,6 +47,9 @@ public class Scanner {
                tempStorage.add(tempString);
             }
         }
+        if (isTableNew(tempString, game)) {
+
+        }
         try (PrintStream outputFile = new PrintStream(new FileOutputStream(pathOutput))) {
             for (int i = tempStorage.size() - 1; i >= 0; i++) {
                 outputFile.println(tempStorage.get(i));
@@ -51,6 +57,18 @@ public class Scanner {
         } catch (IOException exc) {
             System.out.println("I/O Error: " + exc);
         }
+    }
+
+    public boolean isTableNew(final String tempString, Game game) {
+        if (game.getTables().size() != 0) {
+            String tempHexKey = tempString.substring(19);
+            for (Table table : game.getTables()) {
+                if (tempHexKey.equals(table.getHexKey())) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public boolean isStartOfDistribution(final String tempString) {
