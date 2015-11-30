c61ff028af6119e0b9ef0d306361a2fbbf3dc708
NikaStark <k7a3r5@gmail.com>
2015-11-30 23:32:22 +0200

    DistributionTest - refactor because of add new functionality in to Distribution

--- tests/engine/models/DistributionTest.java
+++ tests/engine/models/DistributionTest.java
@@ -14,7 +14,7 @@ public class DistributionTest {
     @Test
     public void testGetCardsOfPlayer() throws Exception {
         final Card[] inputCards = new Card[]{new Card(10, Suits.Diamonds), new Card(10, Suits.Spades)};
-        final Distribution distribution = new Distribution(inputCards);
+        final Distribution distribution = new Distribution(inputCards, null);
         final Card[] actualCards = distribution.getCardsOfPlayer();
         assertArrayEquals(inputCards, actualCards);
     }
@@ -22,7 +22,7 @@ public class DistributionTest {
     @Test
     public void testGetFlopCards() throws Exception {
         final Card[] inputPlayersCards = new Card[]{new Card(10, Suits.Diamonds), new Card(10, Suits.Spades)};
-        final Distribution distribution = new Distribution(inputPlayersCards);
+        final Distribution distribution = new Distribution(inputPlayersCards, null);
         final Card[] inputFlopCards = new Card[]{new Card(3, Suits.Diamonds), new Card(4, Suits.Diamonds), new Card(5, Suits.Diamonds)};
         distribution.setFlopCards(inputFlopCards);
         final Card[] actualCards = distribution.getFlopCards();
@@ -32,7 +32,7 @@ public class DistributionTest {
     @Test
     public void testGetTurnCard() throws Exception {
         final Card[] inputPlayersCards = new Card[]{new Card(10, Suits.Diamonds), new Card(10, Suits.Spades)};
-        final Distribution distribution = new Distribution(inputPlayersCards);
+        final Distribution distribution = new Distribution(inputPlayersCards, null);
         final Card inputTurnCards = new Card(7, Suits.Clubs);
         distribution.setTurnCard(inputTurnCards);
         final Card actualCards = distribution.getTurnCard();
@@ -42,7 +42,7 @@ public class DistributionTest {
     @Test
     public void testGetRiverCard() throws Exception {
         final Card[] inputPlayersCards = new Card[]{new Card(10, Suits.Diamonds), new Card(10, Suits.Spades)};
-        final Distribution distribution = new Distribution(inputPlayersCards);
+        final Distribution distribution = new Distribution(inputPlayersCards, null);
         final Card inputRiverCards = new Card(9, Suits.Hertz);
         distribution.setRiverCard(inputRiverCards);
         final Card actualCards = distribution.getRiverCard();
@@ -52,7 +52,7 @@ public class DistributionTest {
     @Test
     public void testSetFlopCardsWhenAlreadyAssigned() throws Exception {
         final Card[] inputPlayersCards = new Card[]{new Card(10, Suits.Diamonds), new Card(10, Suits.Spades)};
-        final Distribution distribution = new Distribution(inputPlayersCards);
+        final Distribution distribution = new Distribution(inputPlayersCards, null);
         final Card[] inputFlopCards = new Card[]{new Card(3, Suits.Diamonds), new Card(4, Suits.Diamonds), new Card(5, Suits.Diamonds)};
         distribution.setFlopCards(inputFlopCards);
         try {
@@ -64,7 +64,7 @@ public class DistributionTest {
     @Test
     public void testSetTurnCardAlreadyAssigned() throws Exception {
         final Card[] inputPlayersCards = new Card[]{new Card(10, Suits.Diamonds), new Card(10, Suits.Spades)};
-        final Distribution distribution = new Distribution(inputPlayersCards);
+        final Distribution distribution = new Distribution(inputPlayersCards, null);
         final Card inputTurnCards = new Card(7, Suits.Clubs);
         distribution.setTurnCard(inputTurnCards);
         try {
@@ -76,7 +76,7 @@ public class DistributionTest {
     @Test
     public void testSetRiverCardAlreadyAssigned() throws Exception {
         final Card[] inputPlayersCards = new Card[]{new Card(10, Suits.Diamonds), new Card(10, Suits.Spades)};
-        final Distribution distribution = new Distribution(inputPlayersCards);
+        final Distribution distribution = new Distribution(inputPlayersCards, null);
         final Card inputRiverCards = new Card(9, Suits.Hertz);
         distribution.setRiverCard(inputRiverCards);
         try {
@@ -94,7 +94,7 @@ public class DistributionTest {
         expectedAnswer.removeAll(new ArrayList<>(Arrays.asList(inputPlayersCards)));
         expectedAnswer.removeAll(new ArrayList<>(Arrays.asList(inputFlopCards)));
         expectedAnswer.remove(inputTurnCad);
-        final Distribution distribution = new Distribution(inputPlayersCards);
+        final Distribution distribution = new Distribution(inputPlayersCards, null);
         distribution.setFlopCards(inputFlopCards);
         distribution.setTurnCard(inputTurnCad);
         final ArrayList<Card> actualAnswer = distribution.getCurrentDeck();
@@ -106,7 +106,7 @@ public class DistributionTest {
         final Card[] inputPlayersCards = new Card[]{new Card(4, Suits.Hertz), new Card(6, Suits.Spades)};
         final Card[] inputFlopCards = new Card[]{new Card(7, Suits.Clubs), new Card(2, Suits.Diamonds), new Card(9, Suits.Hertz)};
         final Card[] expectedAnswer = inputFlopCards;
-        final Distribution distribution = new Distribution(inputPlayersCards);
+        final Distribution distribution = new Distribution(inputPlayersCards, null);
         final Distribution.StreetPoker inputStreetPoker = Distribution.StreetPoker.Flop;
         distribution.setFlopCards(inputFlopCards);
         final Card[] actualAnswer = distribution.currentKnownCards(inputStreetPoker);
@@ -119,7 +119,7 @@ public class DistributionTest {
         final Card[] inputFlopCards = new Card[]{new Card(7, Suits.Clubs), new Card(2, Suits.Diamonds), new Card(9, Suits.Hertz)};
         final Card[] expectedAnswer = new Card[]{new Card(7, Suits.Clubs), new Card(10, Suits.Diamonds), new Card(7, Suits.Clubs),
                 new Card(2, Suits.Diamonds), new Card(9, Suits.Hertz)};
-        final Distribution distribution = new Distribution(inputPlayersCards);
+        final Distribution distribution = new Distribution(inputPlayersCards, null);
         final Distribution.StreetPoker inputStreetPoker = Distribution.StreetPoker.Flop;
         distribution.setFlopCards(inputFlopCards);
         final Card[] actualAnswer = distribution.allCurrentKnownCards(inputStreetPoker);
@@ -131,7 +131,7 @@ public class DistributionTest {
         final Card[] inputPlayersCards = new Card[]{new Card(7, Suits.Clubs), new Card(10, Suits.Diamonds)};
         final Distribution.StreetPoker inputStreetPoker = Distribution.StreetPoker.Turn;
         final int expectedAnswer = 6;
-        final Distribution distribution = new Distribution(inputPlayersCards);
+        final Distribution distribution = new Distribution(inputPlayersCards, null);
         final int actualAnswer = distribution.counterKnownCards(inputStreetPoker);
         assertEquals(expectedAnswer, actualAnswer);
     }
@@ -140,7 +140,7 @@ public class DistributionTest {
     public void testCurrentStreetPoker() throws Exception {
         final Card[] inputPlayersCards = new Card[]{new Card(7, Suits.Clubs), new Card(10, Suits.Diamonds)};
         final Distribution.StreetPoker expectedAnswer = Distribution.StreetPoker.PreFlop;
-        final Distribution distribution = new Distribution(inputPlayersCards);
+        final Distribution distribution = new Distribution(inputPlayersCards, null);
         final Distribution.StreetPoker actualAnswer = distribution.currentStreetPoker();
         assertEquals(expectedAnswer, actualAnswer);
     }
