d31f53ce2728fa1106f0bdc08c9462495d45de23
NikaStark <k7a3r5@gmail.com>
2015-12-02 19:44:24 +0200

    ScannerTest - adding new tests and fixed bugs in old tests

--- tests/inputer/ScannerTest.java
+++ tests/inputer/ScannerTest.java
@@ -13,6 +13,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.RandomAccessFile;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -21,14 +22,55 @@ public class ScannerTest {
     public static File tempFile = new File("temp.0");
 
     public static String[] input = new String[] {
-            "VC++ __active_heap = 1",
-            "HeapQueryInformation=2",
-            "GetProcessAffinityMask=0xF",
             "maxTablePlayers = 6",
-            "GetProcessAffinityMask=0xF",
-            "tryLock supported",
-            "PID=6072",
-            " '*' 'E' 2000, 49000, 1000, 100"
+            "Game #144453195279 0006098E",
+            "[2015/11/27 14:54:44]",
+            " 'Q' 1000",
+            " '_' 0",
+            " ' ' 0",
+            "9314 'Q' 1000 - 0",
+            "  sit0",
+            "::: 10h",
+            "  nCards=2",
+            "::: 7s",
+            "  sit1",
+            "------ 0006098E",
+            "  nCards=2",
+            "  sit3",
+            "  nCards=2",
+            "  sit4",
+            "  nCards=2",
+            "  sit5",
+            "  nCards=2",
+            "  dealerPos=0",
+            "myCards.changed=1",
+            ":::TableViewImpl::updateMyCard() 10h (0) [6098E]",
+            ":::TableViewImpl::updateMyCard() 7s (1) [6098E]",
+            "  sit0",
+            "  nCards=2",
+            "  sit1",
+            "  nCards=2",
+            "  sit3",
+            "  nCards=2",
+            "  sit4",
+            "  nCards=2",
+            "  sit5",
+            "  nCards=2",
+            ":::TableViewImpl::updateMyCard() 10h (0) [6098E]",
+            ":::TableViewImpl::updateMyCard() 7s (1) [6098E]",
+            "[2015/11/27 14:54:45]",
+            "box[ 0 ] 0",
+            " act 'c' -1",
+            " act 'F' -1",
+            "box[ 2 ] 0",
+            " act 'c' -1",
+            "box[ 3 ] 0",
+            " act 'c' -1",
+            " act 'C' -1",
+            "[2015/11/27 14:54:46]",
+            " 'F' 0",
+            " 'c' 0",
+            " '*' 'E' 2000, 40000, 1000, 100"
     };
 
     @BeforeClass
@@ -53,46 +95,83 @@ public class ScannerTest {
     }
 
     @Test
-    public void testSearchMaxTablePlayers() throws Exception {
+    public void testScanner() throws Exception {
         final Scanner scanner = new Scanner();
-        final int expectedAnswer = 6;
-        int actualResult = -1;
+        final String[] expectedAnswer = new String[input.length - 1];
+        for (int i = 0; i < expectedAnswer.length; i++) {
+            expectedAnswer[i] = input[input.length - i - 1];
+        }
+        final List<String> tempList = scanner.scanner(tempFile);
+        final String[] actualAnswer = tempList.toArray(new String[tempList.size()]);
+        assertArrayEquals(expectedAnswer,actualAnswer);
+    }
+
+    @Test
+    public void testReadingLogs() throws Exception {
+        final Scanner scanner = new Scanner();
+        final String[] expectedAnswer = new String[input.length - 1];
+        for (int i = 0; i < expectedAnswer.length; i++) {
+            expectedAnswer[i] = input[input.length - i - 1];
+        }
+        String[] actualAnswer;
         try (RandomAccessFile randomAccessFile = new RandomAccessFile(tempFile, "r")) {
-            actualResult = scanner.searchMaxTablePlayers(randomAccessFile);
+            List<String> tempList = scanner.readingLogs(randomAccessFile);
+            actualAnswer = tempList.toArray(new String[tempList.size()]);
+        }
+        assertArrayEquals(expectedAnswer,actualAnswer);
+    }
+
+    @Test
+    public void testSearchBigBlind() throws Exception {
+        final Scanner scanner = new Scanner();
+        final int expectedAnswer = 1000;
+        int actualAnswer = -1;
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(tempFile, "r")) {
+            actualAnswer = scanner.searchBigBlind(randomAccessFile);
         } catch (IOException exc) {
             System.out.println("I/O Error: " + exc);
         }
-        assertEquals(expectedAnswer, actualResult);
+        assertEquals(expectedAnswer,actualAnswer);
+    }
+
+    @Test
+    public void testSearchMaxTablePlayers() throws Exception {
+        final Scanner scanner = new Scanner();
+        final int expectedAnswer = 6;
+        int actualAnswer = -1;
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(tempFile, "r")) {
+            actualAnswer = scanner.searchMaxTablePlayers(randomAccessFile);
+        } catch (IOException exc) {
+            System.out.println("I/O Error: " + exc);
+        }
+        assertEquals(expectedAnswer, actualAnswer);
     }
 
     @Test
     public void testIsTableNewWhenTableAbsent() throws Exception {
-        final Game game = new Game();
         final Scanner scanner = new Scanner();
         final String tempString = "Game #142837817463 005D0A28";
-        final boolean actualAnswer = scanner.isTableNew(tempString, game);
+        final boolean actualAnswer = scanner.isTableNew(tempString);
         assertTrue(actualAnswer);
     }
 
     @Test
     public void testIsTableNewWhenTableNew() throws Exception {
-        final Game game = new Game();
         final Scanner scanner = new Scanner();
-        final Table table = new Table("321DF898", 6);
-        game.addTables(table);
-        final String tempString = "Game #142837817463 005D0A28";
-        final boolean actualAnswer = scanner.isTableNew(tempString, game);
+        final Table table = new Table("321DF898", 10, 6);
+        Game.tables.add(table);
+        final String tempString = "Game #142837817463 005D0A29";
+        final boolean actualAnswer = scanner.isTableNew(tempString);
         assertTrue(actualAnswer);
     }
 
     @Test
     public void testIsTableNewWhenTableNotNew() throws Exception {
-        final Game game = new Game();
         final Scanner scanner = new Scanner();
-        final Table table = new Table("005D0A28", 6);
-        game.addTables(table);
+        final Table table = new Table("005D0A28", 10,  6);
+        Game.tables.add(table);
         final String tempString = "Game #142837817463 005D0A28";
-        final boolean actualResult = scanner.isTableNew(tempString, game);
+        final boolean actualResult = scanner.isTableNew(tempString);
         assertFalse(actualResult);
     }
 
