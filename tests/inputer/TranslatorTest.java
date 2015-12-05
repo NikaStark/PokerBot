effc30b3875bf66fbe44b90f32632843f707cfb5
NikaStark <k7a3r5@gmail.com>
2015-12-05 23:43:01 +0200

    TranslatorTest - create

---
+++ tests/inputer/TranslatorTest.java
@@ -0,0 +1,33 @@
+package inputer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TranslatorTest {
+
+    @Test
+    public void testWhatStreetNow() throws Exception {
+
+    }
+
+    @Test
+    public void testDestroyUnnecessaryInfo() throws Exception {
+
+    }
+
+    @Test
+    public void testSearchPointer() throws Exception {
+
+    }
+
+    @Test
+    public void testIsDistributionNew() throws Exception {
+
+    }
+
+    @Test
+    public void testSearchTable() throws Exception {
+
+    }
+}
