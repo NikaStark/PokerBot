9e08886b665b10f13bfe9603b312428eb2227b21
NikaStark <k7a3r5@gmail.com>
2016-01-14 23:59:48 +0200

    Translator - add working method of creating model Table

--- src/inputer/Translator.java
+++ src/inputer/Translator.java
@@ -1,19 +1,29 @@
 package inputer;
 
-import engine.models.Distribution;
-import engine.models.Game;
-import engine.models.Table;
-import engine.models.exceptions.AbstractPokerBotException;
-import engine.models.exceptions.NotFoundTableException;
+import engine.models.*;
+import engine.models.exceptions.*;
 
 import java.util.List;
 
 public class Translator {
 
     public void translator(List<String> tempStorage) {
-        tempStorage = whatStreetNow(tempStorage) == Distribution.StreetPoker.PreFlop ?
-        destroyReiterateInfo(tempStorage) : destroyUnnecessaryInfo(tempStorage);
+        tempStorage = !isDistributionNew(tempStorage.get(1)) ?
+                whatStreetNow(tempStorage) == Distribution.StreetPoker.PreFlop ?
+                        destroyReiterateInfo(tempStorage)
+                        : destroyUnnecessaryInfo(tempStorage)
+                : destroyReiterateInfo(tempStorage);
+//        if (!isDistributionNew(tempStorage.get(1))) {
+//            tempStorage = whatStreetNow(tempStorage) == Distribution.StreetPoker.PreFlop ?
+//                    destroyReiterateInfo(tempStorage) : destroyUnnecessaryInfo(tempStorage);
+//        } else {
+//            tempStorage = destroyReiterateInfo(tempStorage);
+//        }
+        try {
+            reader(tempStorage);
+        } catch (AbstractPokerBotException exc) {
 
+        }
     }
 
     public Distribution.StreetPoker whatStreetNow(List<String> tempStorage) {
@@ -35,7 +45,8 @@ public class Translator {
 
     public List<String> destroyReiterateInfo(List<String> tempStorage) {
         for (int i = 0; i < tempStorage.size(); i++) {
-            if (tempStorage.get(i).substring(0, 3).equals(":::")) {
+            String subString = tempStorage.get(i).substring(0, 3);
+            if (subString.equals(":::")) {
                 if (tempStorage.get(i).length() <= 7) {
                     tempStorage.remove(i--);
                 } else {
@@ -46,6 +57,8 @@ public class Translator {
                     }
                     tempStorage.remove(i--);
                 }
+            } else if (subString.equals("  s")) {
+                tempStorage.remove(i--);
             }
         }
         return tempStorage;
@@ -87,5 +100,79 @@ public class Translator {
         }
         throw new NotFoundTableException();
     }
+
+    public void reader(List<String> tempStorage) throws
+            NotFoundTableException, IllegalSuitException, NotFoundDistributionException, ReassigningFieldException {
+        Card[] playerCards = new Card[Game.AMOUNT_CARDS_AT_PLAYER];
+        Card[] flopCards = new Card[Game.AMOUNT_CARDS_AT_FLOP];
+        Table currentTable = searchTable(tempStorage.get(0).substring(19));
+        for (int i = 0; i < tempStorage.size(); i++) {
+            switch (tempStorage.get(i).substring(0, 5)) {
+                case "  sit":
+                    initializePlayer(tempStorage, currentTable, i);
+                    break;
+                case "  dea":
+                    currentTable.setDealerPos(Integer.parseInt(tempStorage.get(i).substring(12, 13)));
+                    break;
+                case ":::Ta":
+                    String line = tempStorage.get(i);
+                    int numberOfCard = Integer.parseInt(line.substring(
+                            line.lastIndexOf('(') + 1, line.lastIndexOf('(') + 2));
+                    if (line.substring(24, 26).equals("My")) {
+                        playerCards[numberOfCard] = searchCard(line);
+                        if (numberOfCard == Game.AMOUNT_CARDS_AT_PLAYER - 1) {
+                            currentTable.setCurrentDistribution(
+                                    new Distribution(playerCards, tempStorage.get(0).substring(6, 18)));
+                        }
+                    } else {
+                        if (numberOfCard <= 2) {
+                            playerCards[numberOfCard] = searchCard(line);
+                            if (numberOfCard == Game.AMOUNT_CARDS_AT_FLOP - 1) {
+                                currentTable.getCurrentDistribution().setFlopCards(flopCards);
+                            }
+                        } else if (numberOfCard == 3) {
+                            currentTable.getCurrentDistribution().setTurnCard(searchCard(line));
+                        } else if (numberOfCard == 4) {
+                            currentTable.getCurrentDistribution().setRiverCard(searchCard(line));
+                        }
+                    }
+
+                    break;
+            }
+        }
+    }
+
+    public boolean initializePlayer(List<String> tempStorage, Table currentTable, int i) {
+        try {
+            String line = tempStorage.get(i);
+            int numberPlayer = Integer.parseInt(line.substring(5, 6));
+            if (currentTable.getIndexFirstPlayerNotNull() != numberPlayer) {
+                do {
+                    currentTable.getPlayers()[currentTable.getIndexFirstPlayerNotNull()] = new Player();
+                } while (currentTable.getIndexFirstPlayerNotNull() == numberPlayer);
+            }
+            currentTable.getPlayers()[currentTable.getIndexFirstPlayerNotNull()] =
+                    tempStorage.get(i + 1).substring(0, 3).equals(":::") ?
+                            new Player(true) :
+                            new Player(false, true);
+            return true;
+        } catch (AbsentInfinitePlayerException exc) {
+            System.out.println("Error: " + exc);
+            return false;
+        }
+    }
+
+    public Card searchCard(String line) throws IllegalSuitException {
+        int numericValue = Integer.parseInt(line.substring(31, 32));
+        char suit;
+        if (numericValue == 1) {
+            numericValue = Integer.parseInt(line.substring(32, 33)) + 10;
+            suit = line.charAt(33);
+        } else {
+            suit = line.charAt(32);
+        }
+        return new Card(numericValue, suit);
+    }
+
 
 }
