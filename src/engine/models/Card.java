ab406f4508b387c1adbe51b0ea9c5fb0dc637865
NikaStark <k7a3r5@gmail.com>
2016-01-18 06:32:28 +0200

    Card - modified constructors, rename "numericValue" to "Dignity"

--- src/engine/models/Card.java
+++ src/engine/models/Card.java
@@ -6,45 +6,44 @@ import engine.models.exceptions.IllegalSuitException;
 
 public class Card {
 
-    private final int numericValue;
-
+    private final int dignity;
     private final Suits suit;
 
-    public Card(final int numericValue, final Suits suit) {
-        this.numericValue = numericValue;
+    public Card(final int dignity, final Suits suit) {
+        this.dignity = dignity;
         this.suit = suit;
     }
 
-    public Card(final int numericValue, final char suit)  throws IllegalSuitException {
-        if (suit == 's'){
-            this.suit = Suits.Spades;
-        } else if (suit == 'h'){
-            this.suit = Suits.Hertz;
-        } else if (suit == 'c'){
-            this.suit = Suits.Clubs;
-        } else if (suit == 'd'){
-            this.suit = Suits.Diamonds;
+    public Card(final int dignity, final char suit)  throws IllegalSuitException {
+        if (suit == 's' || suit == 'S'){
+            this.suit = Suits.SPADES;
+        } else if (suit == 'h' || suit == 'H'){
+            this.suit = Suits.HERTZ;
+        } else if (suit == 'c' || suit == 'C'){
+            this.suit = Suits.CLUBS;
+        } else if (suit == 'd' || suit == 'D'){
+            this.suit = Suits.DIAMONDS;
         } else {
             throw new IllegalSuitException();
         }
-        this.numericValue = numericValue;
+        this.dignity = dignity;
     }
 
     public Card(final HighCard symbolicValue, final Suits suit) {
         if (symbolicValue == HighCard.Jack){
-            this.numericValue = Game.MAX_NUMERIC_VALUE_CARD + 1;
+            this.dignity = Game.MAX_NUMERIC_VALUE_CARD + 1;
         } else if (symbolicValue == HighCard.Queen){
-            this.numericValue = Game.MAX_NUMERIC_VALUE_CARD + 2;
+            this.dignity = Game.MAX_NUMERIC_VALUE_CARD + 2;
         } else if (symbolicValue == HighCard.King){
-            this.numericValue = Game.MAX_NUMERIC_VALUE_CARD + 3;
+            this.dignity = Game.MAX_NUMERIC_VALUE_CARD + 3;
         } else {
-            this.numericValue = Game.MAX_NUMERIC_VALUE_CARD + 4;
+            this.dignity = Game.MAX_NUMERIC_VALUE_CARD + 4;
         }
         this.suit = suit;
     }
 
-    public int getNumericValue() {
-        return numericValue;
+    public int getDignity() {
+        return dignity;
     }
 
     public Suits getSuit() {
@@ -58,7 +57,7 @@ public class Card {
         }
         if (this.getClass() == object.getClass()) {
             Card card = (Card) object;
-            if (this.numericValue == card.numericValue && this.suit == card.suit) {
+            if (this.dignity == card.dignity && this.suit == card.suit) {
                 return true;
             }
         }
@@ -67,7 +66,7 @@ public class Card {
 
     @Override
     public String toString() {
-        return this.numericValue + this.suit.toString();
+        return this.dignity + this.suit.toString();
     }
 
 }
