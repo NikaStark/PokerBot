912abedcd9be4ef56e4d4493bb94c9111faed563
NikaStark <k7a3r5@gmail.com>
2016-01-22 16:53:38 +0200

    Suits - add methods "create"

--- src/engine/models/Enums/Suits.java
+++ src/engine/models/Enums/Suits.java
@@ -1,19 +1,48 @@
 package engine.models.Enums;
 
+import engine.models.exceptions.IllegalSuitException;
+
 public enum Suits {
 
-    Spades, Hertz, Clubs, Diamonds;
+    SPADES,
+    HERTZ,
+    CLUBS,
+    DIAMONDS;
+
+    public static Suits create(char suit) throws IllegalSuitException{
+        switch (suit) {
+            case 's':
+            case 'S': return Suits.SPADES;
+            case 'h':
+            case 'H': return Suits.HERTZ;
+            case 'c':
+            case 'C': return Suits.CLUBS;
+            case 'd':
+            case 'D': return Suits.DIAMONDS;
+            default: throw new IllegalSuitException();
+        }
+    }
+
+    public static Suits create(String suit) throws IllegalSuitException{
+        switch (suit.toLowerCase()) {
+            case "spades": return Suits.SPADES;
+            case "hertz": return Suits.HERTZ;
+            case "clubs": return Suits.CLUBS;
+            case "diamonds": return Suits.DIAMONDS;
+            default: throw new IllegalSuitException();
+        }
+    }
 
     @Override
     public String toString() {
-        if (this == Spades) {
-            return "Spades";
-        } else if (this == Hertz) {
-            return "Hertz";
-        } else if (this == Clubs) {
-            return "Clubs";
+        if (this == SPADES) {
+            return "SPADES";
+        } else if (this == HERTZ) {
+            return "HERTZ";
+        } else if (this == CLUBS) {
+            return "CLUBS";
         } else {
-            return "Diamonds";
+            return "DIAMONDS";
         }
     }
 
