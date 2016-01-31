2224cc4dfced88206bbcab70b5782741538d4518
NikaStark <k7a3r5@gmail.com>
2016-01-31 07:52:49 +0200

    Main - method to render start table chances in one thread

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,62 @@
 //Created by NikaStark on 28.07.2015.
 
+import engine.controllers.calculator.Calculator;
+import engine.models.Card;
+import engine.models.Distribution;
+import engine.models.Game;
 import inputer.Scanner;
 import inputer.Translator;
 
-import java.io.File;
+import java.io.*;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
-        final String pathInputOrigin = "C:\\Users\\Alex\\AppData\\Local\\PokerStars\\PokerStars.log.0";
-        final File fileInput = new File(pathInputOrigin);
-        Scanner scanner = new Scanner();
-        List<String> tempStorage = scanner.scanner(fileInput);
-        Translator translator = new Translator();
-        translator.translator(tempStorage);
+        long t1=System.nanoTime();
+        final String path = "C:\\Users\\Alex\\Desktop\\primalCalculation2.txt";
+        final Calculator calculator = new Calculator();
+        try(PrintStream outputFile = new PrintStream(new FileOutputStream(path))) {
+            for (int i = 0; i < Game.DECK_OF_CARDS.length - 1; i++) {
+                for (int j = i + 1; j < Game.DECK_OF_CARDS.length; j++) {
+                    Distribution distribution = new Distribution(new Card[]{Game.DECK_OF_CARDS[i], Game.DECK_OF_CARDS[j]}, null);
+                    outputFile.print(Game.DECK_OF_CARDS[i] + " " + Game.DECK_OF_CARDS[j] + " ");
+                    for (int number : calculator.getTableChances(distribution)) {
+                        outputFile.print(number + "  ");
+                    }
+                    outputFile.println();
+                }
+            }
+        } catch (IOException exc) {
+            System.out.println("I/O Error: " + exc);
+        }
+        long t2 = System.nanoTime();
+        long elapsedTime = t2-t1;
+        System.out.println("Elapsed time was "+elapsedTime+" ns");
     }
+
+//    public static void main(String[] args) throws Exception {
+//
+//        final String pathInputOrigin = "C:\\Users\\Alex\\AppData\\Local\\PokerStars\\PokerStars.log.0";
+//        final File fileInput = new File(pathInputOrigin);
+//        Scanner scanner = new Scanner();
+//        List<String> tempStorage = scanner.scanner(fileInput);
+//        Translator translator = new Translator();
+//        translator.translator(tempStorage);
+//    }
 
 
 //    public static void main(String[] args) throws AbstractPokerBotException {
 //        long t1=System.nanoTime();
 //        final Calculator calculator = new Calculator();
 //        final Card[] inputCardsOfPlayer = new Card[]{
-//                new Card(6, Suits.Spades),
-//                new Card(5, Suits.Spades)
+//                new Card(6, Suits.SPADES),
+//                new Card(5, Suits.SPADES)
 //        };
 //        final Card[] inputFlopCards = new Card[]{
-//                new Card(7, Suits.Hertz),
-//                new Card(2, Suits.Clubs),
-//                new Card(HighCard.King, Suits.Diamonds)
+//                new Card(7, Suits.HERTZ),
+//                new Card(2, Suits.CLUBS),
+//                new Card(HighCard.King, Suits.DIAMONDS)
 //        };
 //        final Distribution distribution = new Distribution(inputCardsOfPlayer);
 //        distribution.setFlopCards(inputFlopCards);
