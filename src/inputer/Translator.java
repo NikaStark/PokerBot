d9faf53c79414dacc00f853674c74b6ff9152299
NikaStark <k7a3r5@gmail.com>
2016-02-17 13:06:28 +0200

    Translator - change modificator access, refactor and optimize reader method and create some new methods which are parts of reader

--- src/inputer/Translator.java
+++ src/inputer/Translator.java
@@ -13,20 +13,10 @@ public class Translator {
                         destroyReiterateInfo(tempStorage)
                         : destroyUnnecessaryInfo(tempStorage)
                 : destroyReiterateInfo(tempStorage);
-//        if (!isDistributionNew(tempStorage.get(1))) {
-//            tempStorage = whatStreetNow(tempStorage) == Distribution.StreetPoker.PRE_FLOP ?
-//                    destroyReiterateInfo(tempStorage) : destroyUnnecessaryInfo(tempStorage);
-//        } else {
-//            tempStorage = destroyReiterateInfo(tempStorage);
-//        }
-        try {
-            reader(tempStorage);
-        } catch (AbstractPokerBotException exc) {
-
-        }
+         reader(tempStorage);
     }
 
-    public Distribution.StreetPoker whatStreetNow(List<String> tempStorage) {
+    protected Distribution.StreetPoker whatStreetNow(List<String> tempStorage) {
         for (int i = tempStorage.size() - 1; i >= 0; i--) {
             String currentLine = tempStorage.get(i);
             if (currentLine.substring(18, currentLine.indexOf(' ')).equals("updateMyCard()")) {
@@ -40,10 +30,10 @@ public class Translator {
                 }
             }
         }
-        return null; //TODO Bad practice, maybe do a throw exception
+        throw new RuntimeException();
     }
 
-    public List<String> destroyReiterateInfo(List<String> tempStorage) {
+    protected List<String> destroyReiterateInfo(List<String> tempStorage) {
         for (int i = 0; i < tempStorage.size(); i++) {
             String subString = tempStorage.get(i).substring(0, 3);
             if (subString.equals(":::")) {
@@ -64,13 +54,12 @@ public class Translator {
         return tempStorage;
     }
 
-    public List<String> destroyUnnecessaryInfo(List<String> tempStorage) {
+    protected List<String> destroyUnnecessaryInfo(List<String> tempStorage) {
         tempStorage.removeAll(tempStorage.subList(0, searchPointer(tempStorage) + 1));
         return tempStorage;
     }
 
-    public int searchPointer(List<String> tempStorage) {
-        int temp = -1;
+    protected int searchPointer(List<String> tempStorage) {
         for (int i = tempStorage.size() - 1; i >= 0; i--) {
             String subString = tempStorage.get(i).substring(1, 4);
             if (subString.equals("'*'") || subString.equals("'C'") || subString.equals("'E'")) {
@@ -79,21 +68,21 @@ public class Translator {
                 }
             }
         }
-        return temp; //TODO Bad practice, maybe do a throw exception
+        throw new RuntimeException();
     }
 
-    public boolean isDistributionNew(final String line) {
+    protected boolean isDistributionNew(final String line) {
         String numberOfDistribution = line.substring(6, 18);
         String hexKey = line.substring(19);
         try {
             return !searchTable(hexKey).getCurrentDistribution().getNumberOfDistribution().equals(numberOfDistribution);
-        } catch (AbstractPokerBotException exc) {
+        } catch (NotFoundTableException exc) {
             return false;
         }
     }
 
-    public Table searchTable(final String hexKey) throws NotFoundTableException {
-        for (Table table : Game.tables) {
+    protected Table searchTable(final String hexKey) throws NotFoundTableException {
+        for (Table table : Game.TABLES) {
             if (table.getHexKey().equals(hexKey)) {
                 return table;
             }
@@ -101,100 +90,124 @@ public class Translator {
         throw new NotFoundTableException();
     }
 
-    public void reader(List<String> tempStorage) throws
-            NotFoundTableException, IllegalSuitException, NotFoundDistributionException, ReassigningFieldException {
-        Card[] playerCards = new Card[Game.AMOUNT_CARDS_AT_PLAYER];
-        Card[] flopCards = new Card[Game.AMOUNT_CARDS_AT_FLOP];
-        Table currentTable = searchTable(tempStorage.get(0).substring(19));
-        for (int i = 0; i < tempStorage.size(); i++) {
-            switch (tempStorage.get(i).substring(0, 5)) {
-                case "  sit":
-                    initializePlayer(tempStorage, currentTable, i);
-                    break;
-                case "  dea":
-                    currentTable.getCurrentDistribution().setDealerPos(Integer.parseInt(tempStorage.get(i).substring(12, 13)));
-                    break;
-                case ":::Ta":   // TODO Maybe do creating player and flop cards in a steps(First card - creating array, next - get creating array at last step and put new card...)
-                    String line = tempStorage.get(i);
-                    int numberOfCard = Integer.parseInt(line.substring(
-                            line.lastIndexOf('(') + 1, line.lastIndexOf('(') + 2));
-                    if (line.substring(24, 26).equals("My")) {
-                        playerCards[numberOfCard] = searchCard(line);
-                        if (numberOfCard == Game.AMOUNT_CARDS_AT_PLAYER - 1) {
-                            currentTable.setCurrentDistribution(
-                                    new Distribution(playerCards, tempStorage.get(0).substring(6, 18)));
-                        }
-                    } else {
-                        if (numberOfCard <= 2) {
-                            playerCards[numberOfCard] = searchCard(line);
-                            if (numberOfCard == Game.AMOUNT_CARDS_AT_FLOP - 1) {
-                                currentTable.getCurrentDistribution().setFlopCards(flopCards);
-                            }
-                        } else if (numberOfCard == 3) {
-                            currentTable.getCurrentDistribution().setTurnCard(searchCard(line));
-                        } else if (numberOfCard == 4) {
-                            currentTable.getCurrentDistribution().setRiverCard(searchCard(line));
-                        }
-                    }
-                    break;
-                case " 'F' ":
-                    currentTable.getCurrentDistribution().getCurrentPossibleSteps().
-                            put(Distribution.PossibleSteps.FOLD, new Integer[]{0});
-                    break;
-                case " 'c' ":
-                    currentTable.getCurrentDistribution().getCurrentPossibleSteps().
-                            put(Distribution.PossibleSteps.CHECK, new Integer[]{0});
-                    break;
-                case " 'C' ":
-                    currentTable.getCurrentDistribution().getCurrentPossibleSteps().
-                            put(Distribution.PossibleSteps.CALL,
-                                    new Integer[]{Integer.parseInt(tempStorage.get(i).substring(5).trim())});
-                    break;
-                case " '*' ":
-                    String[] valuesOfCurrentPossibleSteps = tempStorage.get(i).substring(9).trim().split(", ");
-                    currentTable.getCurrentDistribution().getCurrentPossibleSteps().
-                            put(Distribution.PossibleSteps.RAISE, new Integer[]{
-                                            Integer.parseInt(valuesOfCurrentPossibleSteps[0]),
-                                            Integer.parseInt(valuesOfCurrentPossibleSteps[1]),
-                                            Integer.parseInt(valuesOfCurrentPossibleSteps[2]),
-                                            Integer.parseInt(valuesOfCurrentPossibleSteps[3])});
-                    break;
-            }
-        }
-        currentTable.getCurrentDistribution().rateMyStack();
-    }
-
-    public boolean initializePlayer(List<String> tempStorage, Table currentTable, int i) {
+    protected void reader(List<String> tempStorage)  {
         try {
-            String line = tempStorage.get(i);
-            int numberPlayer = Integer.parseInt(line.substring(5, 6));
-            if (currentTable.getIndexFirstPlayerNotNull() != numberPlayer) {
-                do {
-                    currentTable.getPlayers()[currentTable.getIndexFirstPlayerNotNull()] = new Player();
-                } while (currentTable.getIndexFirstPlayerNotNull() == numberPlayer);
+            Table currentTable = searchTable(tempStorage.get(0).substring(19));
+            for (String currentLine : tempStorage) {
+                switch (currentLine.substring(0, 5)) {
+                    case "  sit":
+                        initializePlayer(currentLine, currentTable);
+                        break;
+                    case "  dea":
+                        initializeDealerPos(currentLine, currentTable);
+                        break;
+                    case ":::Ta":
+                        initializeCard(currentLine, currentTable);
+                        break;
+                    case " 'F' ":
+                        initializePossibleSteps(currentLine, currentTable, currentLine.charAt(2));
+                        break;
+                    case " 'c' ":
+                        initializePossibleSteps(currentLine, currentTable, currentLine.charAt(2));
+                        break;
+                    case " 'C' ":
+                        initializePossibleSteps(currentLine, currentTable, currentLine.charAt(2));
+                        break;
+                    case " 'E' ":
+                        initializePossibleSteps(currentLine, currentTable, currentLine.charAt(2));
+                        break;
+                    case " '*' ":
+                        initializePossibleSteps(currentLine, currentTable, currentLine.charAt(2));
+                        break;
+                }
             }
-            currentTable.getPlayers()[currentTable.getIndexFirstPlayerNotNull()] =
-                    tempStorage.get(i + 1).substring(0, 3).equals(":::") ?
-                            new Player(true) :
-                            new Player(false, true);
-            return true;
-        } catch (AbsentInfinitePlayerException exc) {
-            System.out.println("Error: " + exc);
-            return false;
+            currentTable.getCurrentDistribution().calculateMyStack();
+            currentTable.getCurrentDistribution().calculateBank(currentTable.getHwnd()); // TODO You must do this only at first street each distribution
+        } catch (NotFoundTableException exc) {
+            throw new RuntimeException("Exception - " + exc);
         }
     }
 
-    public Card searchCard(String line) throws IllegalSuitException {
-        int numericValue = Integer.parseInt(line.substring(31, 32));
+    protected Card searchCard(String line) throws IllegalSuitException {
+        int numericValue = Integer.parseInt(line.substring(33, 34));
         char suit;
         if (numericValue == 1) {
-            numericValue = Integer.parseInt(line.substring(32, 33)) + 10;
-            suit = line.charAt(33);
+            numericValue = Integer.parseInt(line.substring(34, 35)) + 10;
+            suit = line.charAt(35);
         } else {
-            suit = line.charAt(32);
+            suit = line.charAt(34);
         }
         return new Card(numericValue, suit);
     }
 
+    protected void initializePlayer(String line, Table currentTable) {
+        try {
+            int numberPlayer = Integer.parseInt(line.substring(5, 6));
+            if (currentTable.getIndexFirstPlayerNotNull() != numberPlayer) {
+                do {
+                    currentTable.getPlayers()[currentTable.getIndexFirstPlayerNotNull()] = new Player();
+                } while (currentTable.getIndexFirstPlayerNotNull() != numberPlayer);
+            }
+            currentTable.getPlayers()[currentTable.getIndexFirstPlayerNotNull()] = new Player(true);
+        } catch (AbsentNotInitializePlayerException exc) {
+           throw new RuntimeException("Exception - " + exc);
+        }
+    }
+
+    protected void defineMySit(Table currentTable) {
+        try {
+            currentTable.getPlayers()[currentTable.getIndexFirstPlayerNotNull() - 1].setMySit(true);
+        } catch (AbsentNotInitializePlayerException exc) {
+            throw new RuntimeException("Exception - " + exc);
+        }
+    }
+
+    protected void initializeCard(String line, Table currentTable) {
+        int numberOfCard = Integer.parseInt(line.substring(line.lastIndexOf('(') + 1, line.lastIndexOf('(') + 2));
+        try {
+            if (line.substring(24, 26).equals("My")) {
+                currentTable.getCurrentDistribution().addPlayersCard(searchCard(line), numberOfCard);
+                defineMySit(currentTable);
+            } else {
+                if (numberOfCard <= 2) {
+                    currentTable.getCurrentDistribution().addFlopCard(searchCard(line), numberOfCard);
+                } else if (numberOfCard == 3) {
+                    currentTable.getCurrentDistribution().setTurnCard(searchCard(line));
+                } else if (numberOfCard == 4) {
+                    currentTable.getCurrentDistribution().setRiverCard(searchCard(line));
+                }
+            }
+        }catch (IllegalSuitException | ReassigningFieldException exc) {
+            throw new RuntimeException("Exception - " + exc);
+        }
+    }
+
+    protected void initializeDealerPos(String line, Table currentTable) {
+        currentTable.getCurrentDistribution().setDealerPos(Integer.parseInt(line.substring(12, 13)));
+    }
+
+    protected void initializePossibleSteps(String line, Table currentTable, char symbol) {
+        if (symbol == 'F') {
+            currentTable.getCurrentDistribution().getCurrentPossibleSteps().
+                    put(Distribution.PossibleSteps.FOLD, new Integer[]{0});
+        } else if (symbol == 'c') {
+            currentTable.getCurrentDistribution().getCurrentPossibleSteps().
+                    put(Distribution.PossibleSteps.CHECK, new Integer[]{0});
+        } else if (symbol == 'C') {
+            currentTable.getCurrentDistribution().getCurrentPossibleSteps().
+                    put(Distribution.PossibleSteps.CALL, new Integer[]{Integer.parseInt(line.substring(5).trim())});
+        } else if (symbol == 'E') {
+            currentTable.getCurrentDistribution().getCurrentPossibleSteps().
+                    put(Distribution.PossibleSteps.RAISE, new Integer[]{Integer.parseInt(line.substring(5).trim())});
+        } else if (symbol == '*') {
+            String[] valuesOfCurrentPossibleSteps = line.substring(9).trim().split(", ");
+            currentTable.getCurrentDistribution().getCurrentPossibleSteps().
+                    put(Distribution.PossibleSteps.RAISE, new Integer[]{
+                            Integer.parseInt(valuesOfCurrentPossibleSteps[0]),
+                            Integer.parseInt(valuesOfCurrentPossibleSteps[1]),
+                            Integer.parseInt(valuesOfCurrentPossibleSteps[2]),
+                            Integer.parseInt(valuesOfCurrentPossibleSteps[3])});
+        }
+    }
 
 }
